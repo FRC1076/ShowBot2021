3a7ca01cbe66ef5af9c1a034e48776ebbf867655
Soren <sorenfnt@gmail.com>
2021-12-10 16:19:02 -0800

    Create robot.py

---
+++ robot.py
@@ -0,0 +1,20 @@
+import math
+import time
+import wpilib
+from drivetrain import Drivetrain
+from shooter import Shooter
+
+class MyRobot(wpilib.TimedRobot): 
+    def robotInit(self): 
+        SHOOTER_ID = 1
+        
+    def robotPeriodic(self):
+   
+    def teleopInit(self):
+    
+    def teleopPeriodic(self):
+    
+    def autonInit(self):
+    
+    def autonPeriodic(self):
+    
