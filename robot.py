5744a9b5e08bf5313a234d253f812fd3bfed1180
sswangg <s.sophi.w@gmail.com>
2021-12-12 18:16:27 -0500

    Make config.yaml and use it for config

--- robot.py
+++ robot.py
@@ -1,28 +1,40 @@
 import math
 import time
+
 import wpilib
+from wpilib.interfaces import GenericHID
+import yaml
+
 from shooter import Shooter
-from wpilib import interfaces
 
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
-        SHOOTER_ID = 1 # The channel the Spark is on
+        with open('config.yaml') as stream:
+            config = yaml.load(stream, Loader=yaml.CLoader)
 
-        self.shooter = Shooter(SHOOTER_ID)
-        self.controller = wpilib.XboxController(0)
-        # self.right_hand = wpilib.interfaces.GenericHID.Hand.kRightHand
-        # self.left_hand = wpilib.interfaces.GenericHID.Hand.kLeftHand
+        controller_config = config['Controller']
+        channel_config = config['Channels']
 
-        # Change these depending on the controller
-        self.left_trigger_axis = 2 
-        self.right_trigger_axis = 5
+        print(controller_config)
+        self.LEFT_TRIGGER_AXIS = controller_config['left_trigger_axis']
+        self.RIGHT_TRIGGER_AXIS = controller_config['right_trigger_axis']
+        self.A_BUTTON_NUM = controller_config['a_button_num']
+        self.B_BUTTON_NUM = controller_config['b_button_num']
+        self.X_BUTTON_NUM = controller_config['x_button_num']
+        self.Y_BUTTON_NUM = controller_config['y_button_num']
+
+        SHOOTER_CHANNEL = channel_config['shooter_channel']
+        CONTROLLER_PORT = channel_config['controller_port']
+
+        self.operator = Shooter(SHOOTER_CHANNEL)
+        self.controller = GenericHID(CONTROLLER_PORT)
 
     def robotPeriodic(self):
         pass
 
     def teleopInit(self):
-        self.shooter_mod = 1
+        self.shooter_mod = 0
         self.running = 0
 
     def teleopPeriodic(self):
@@ -31,25 +43,25 @@ class MyRobot(wpilib.TimedRobot):
         x reduces the speed, y reduces the speed more, b reduces the speed even more, 
         a reduces the speed the most
         """
-        if self.controller.getRawAxis(self.right_trigger_axis) > 0.95:
-            self.running = 1
-        elif self.controller.getRawAxis(self.left_trigger_axis) > 0.95:
+        if self.controller.getRawAxis(self.LEFT_TRIGGER_AXIS) > 0.95:
             self.running = -0.2
+        elif self.controller.getRawAxis(self.RIGHT_TRIGGER_AXIS) > 0.95:
+            self.running = 1
         else:
             self.running = 0 
 
-        if self.controller.getAButton():
+        if self.controller.getRawButton(self.A_BUTTON_NUM):
             self.shooter_mod = 0.2
-        elif self.controller.getBButton():
+        elif self.controller.getRawButton(self.B_BUTTON_NUM):
             self.shooter_mod = 0.4
-        elif self.controller.getYButton():
+        elif self.controller.getRawButton(self.Y_BUTTON_NUM):
             self.shooter_mod = 0.6
-        elif self.controller.getXButton():
+        elif self.controller.getRawButton(self.X_BUTTON_NUM):
             self.shooter_mod = 0.8
         else:
             self.shooter_mod = 1
 
-        self.shooter.set(self.running * self.shooter_mod)
+        self.operator.set(self.running * self.shooter_mod)
 
     def autonomousInit(self):
         pass
