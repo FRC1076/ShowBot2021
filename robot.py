e879fc86a6cf60d58f00a3d85b3934edd0d5f632
Sophia Wang <s.sophi.w@gmail.com>
2021-12-17 09:53:58 -0500

    Change according to Matthew's code review
    Make all information needed for wiring available in robotInit, make the Shooter class stop using inheritance, change a few formatting things

--- robot.py
+++ robot.py
@@ -2,41 +2,42 @@ import math
 import time
 import wpilib
 from shooter import Shooter
-from wpilib import interfaces
 
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
-        SHOOTER_ID = 1 # The channel the Spark is on
-
-        self.shooter = Shooter(SHOOTER_ID)
-        self.controller = wpilib.XboxController(0)
-        # self.right_hand = wpilib.interfaces.GenericHID.Hand.kRightHand
-        # self.left_hand = wpilib.interfaces.GenericHID.Hand.kLeftHand
+        CONTROLLER_PORT = 0  # Port the controller is connected to, usually 0
+        SHOOTER_CHANNEL = 1  # The channel the motor controller is on
+        SHOOTER_CONTROLLER = wpilib.Spark  # The motor controller we're using
+        
+        self.shooter = Shooter(SHOOTER_CONTROLLER, SHOOTER_CHANNEL)
+        self.controller = wpilib.XboxController(CONTROLLER_PORT)
 
         # Change these depending on the controller
-        self.left_trigger_axis = 2 
-        self.right_trigger_axis = 5
+        self.LEFT_TRIGGER_AXIS = 2
+        self.RIGHT_TRIGGER_AXIS = 5
 
     def robotPeriodic(self):
         pass
 
     def teleopInit(self):
-        self.shooter_mod = 1
-        self.running = 0
+        pass
 
     def teleopPeriodic(self):
+        """ Makes the motor spin
+
+        Gets input from a controller and interprets it as follows:
+        Right trigger -> full speed forwards, left trigger -> a fifth of the speed backwards
+        This input can then be modified by pressing buttons as follows:
+        x reduces the speed, y reduces the speed more, b reduces the speed even more, a reduces the speed the most
         """
-        Makes the motor spin. Right trigger -> 1, left trigger -> -0.2, 
-        x reduces the speed, y reduces the speed more, b reduces the speed even more, 
-        a reduces the speed the most
-        """
-        if self.controller.getRawAxis(self.right_trigger_axis) > 0.95:
-            self.running = 1
-        elif self.controller.getRawAxis(self.left_trigger_axis) > 0.95:
+
+        if self.controller.getRawAxis(self.LEFT_TRIGGER_AXIS) > 0.95:
             self.running = -0.2
+        elif self.controller.getRawAxis(self.RIGHT_TRIGGER_AXIS) > 0.95:
+            self.running = 1
         else:
-            self.running = 0 
+            self.running = 0
 
         if self.controller.getAButton():
             self.shooter_mod = 0.2
@@ -58,6 +59,5 @@ class MyRobot(wpilib.TimedRobot):
         pass
 
 
-# You do need to include these lines for the code to run
-if __name__=="__main__":
+if __name__ == "__main__":
     wpilib.run(MyRobot)
--- shooter.py
+++ shooter.py
@@ -1,5 +1,9 @@
 import wpilib
 
 
-class Shooter(wpilib.Spark):
-    pass
+class Shooter:
+    def __init__(self, motor_controller, channel):
+        self.shooter = motor_controller(channel)
+
+    def set(self, value) -> None:
+        self.shooter.set(value)
